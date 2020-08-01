6cbe3c0330674c706eee7e2cecc9f92a2a179a09
RurouniKenshin24 <68823624+RurouniKenshin24@users.noreply.github.com>
2020-08-01 15:34:49 -0500

    Update BookARoom.java

--- src/BookARoom.java
+++ src/BookARoom.java
@@ -4,6 +4,9 @@ public class BookARoom {
         System.out.println("Welcome, to book a room app!");
         
         
-        //I added this line!!!
+        //I added this line!!!both
+        
+        
+        //For git fetch and git merge command I added tis line. We use both command instead of git pull!!!
     }
 }
